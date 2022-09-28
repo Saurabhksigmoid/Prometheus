a2e6abe96843256506574afdbfaf6e5c557a14c7
Saurabhksigmoid <saurabh.kambli@sigmoidanalytics.com>
2022-09-29 01:20:26 +0530

    app version 2

--- app_info/code.py
+++ app_info/code.py
@@ -1,20 +1,46 @@
-import time
+import prometheus_client
+from prometheus_client import Counter, Gauge
+from flask import Flask, Response
 import psutil
+import sys
+import time
+app = Flask(__name__)
 
-print("Hello World")
+prom = {}
+prom['counter_var'] = Counter('http_request_duration_per_second','shows the http request seconds duration info')
+prom['counter_var1'] = Counter('http_request_total','shows the http request total')
+prom['counter_var2'] = Gauge('memory_usage_total','shows the memory usage info')
+prom['counter_var3'] = Gauge('memory_usage_bytes_total','shows the memory usage in bytes')
+prom['counter_var4'] = Gauge('cpu_usage_percent','shows the cpu percent')
+prom['counter_var5'] = Gauge('cpu_usage_seconds','shows the cpu seconds usage')
+
+def fun():
+  cpu_usage =  psutil.cpu_percent(1)
+  ram_usage =  psutil.virtual_memory()[2]
+  return cpu_usage, ram_usage
  
- 
-def display_usage(cpu_usage,mem_usage,bars=50):
-    cpu_percent = (cpu_usage/100.0)
-    cpu_bar = '▉' * int(cpu_percent*bars) + '-' * (bars - int(cpu_percent*bars))
-    mem_percent = (mem_usage/100.0)
- 
-    mem_bar = '▉' * int(mem_percent*bars) + '-' * (bars - int(mem_percent*bars))
- 
-    print(f"\r CPU Usage: | |{cpu_bar}| {cpu_usage:.2f}%",end="")
-    print(f" Mem Usage: | |{mem_bar}| {mem_usage:.2f}%",end="\r")
- 
-while True:
- 
-   print(f'CPU usage is {psutil.cpu_percent()}, Memory usage is {psutil.virtual_memory().percent},30')
-   time.sleep(10)
+@app.route('/')
+def hello_world():
+
+    prom['counter_var'].inc()
+    prom['counter_var1'].inc()
+    def inner():
+        while True:
+            cpu, rem = fun()
+            prom['counter_var2'].set(rem)
+            prom['counter_var3'].set(rem)
+            prom['counter_var4'].set(cpu)
+            prom['counter_var5'].set(cpu)
+            yield 'Hello, Docker! Your CPU usage is : ' +  str(cpu) + ' -- ' + ' & Memory usage is : ' + str(rem) + '<br/>\n'
+            time.sleep(5)
+    return Response(inner(), mimetype='text/html')
+
+@app.route('/metrics')
+def metrics():
+    c = []
+    for i in prom.items():
+        c.append(prometheus_client.generate_latest())
+    return Response(c, mimetype="text/plan")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True, port=5000)
